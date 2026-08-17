95e40d1ee8fe242295aaf5ed759cccbef6b65974
reuben89raj <reuben.310589@gmail.com>
2026-08-17 16:38:38 +0000

    Deploying to gh-pages from @ reuben89raj/reuben89raj.github.io@dcb98ffcd1aef33c3a542fc14b743e1aad44a740 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -469,7 +469,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/Reuben-Resume_Ind.pdf", "_blank");
+          window.open("/assets/pdf/Reuben_CV_Full.pdf", "_blank");
         },
       },{
         id: 'social-email',
